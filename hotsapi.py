4458a13352901ff93ebc097bd7d092131f3319d5
牧瀬紅莉栖 <saberconer@gmail.com>
2017-09-20 16:18:05 -0500

    Using https

--- hotsapi.py
+++ hotsapi.py
@@ -14,22 +14,22 @@ class hotsapi:
 
     def get_replay_list(self, start_date = None, end_date = None, game_map = None, game_type = None, player = None, min_id = None):
         payload = {'start_date': start_date, 'end_date': end_date, 'game_map': game_map, 'game_type': game_type, 'player': player, 'min_id': min_id}
-        r = requests.get("http://hotsapi.net/api/v1/replays/", params = payload)
+        r = requests.get("https://hotsapi.net/api/v1/replays/", params = payload)
         return self.return_json(r)
 
     def get_replay(self, id):
-        r = requests.get("http://hotsapi.net/api/v1/replays/" + str(id))
+        r = requests.get("https://hotsapi.net/api/v1/replays/" + str(id))
         return self.return_json(r)
 
     def upload_replay(self, file):
         file = {"file": open(file, "rb")}
-        r = requests.post("http://hotsapi.net/api/v1/replays/", files = file)
+        r = requests.post("https://hotsapi.net/api/v1/replays/", files = file)
         return self.return_json(r)
 
     def get_hero_list(self):
-        r = requests.get("http://hotsapi.net/api/v1/heroes/translations")
+        r = requests.get("https://hotsapi.net/api/v1/heroes/translations")
         return self.return_json(r)
 
     def get_map_list(self):
-        r = requests.get("http://hotsapi.net/api/v1/maps/translations")
+        r = requests.get("https://hotsapi.net/api/v1/maps/translations")
         return self.return_json(r)

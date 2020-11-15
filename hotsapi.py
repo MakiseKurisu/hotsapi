2d236611c507bdd492453e3b426913416c6cd890
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2020-11-15 16:14:19 -0600

    Update to the latest API
    Fixes #1

--- hotsapi.py
+++ hotsapi.py
@@ -12,29 +12,44 @@ class hotsapi:
         else:
             return None
 
-    def get_replay_list(self, start_date = None, end_date = None, game_map = None, game_type = None, player = None, min_id = None):
-        payload = {'start_date': start_date, 'end_date': end_date, 'game_map': game_map, 'game_type': game_type, 'player': player, 'min_id': min_id}
-        r = requests.get("https://hotsapi.net/api/v1/replays/", params = payload)
-        return self.return_json(r)
-    
-    def get_paged_replay_list(self, page = None, start_date = None, end_date = None, game_map = None, game_type = None, player = None, min_id = None):
-        payload = {'page': page, 'start_date': start_date, 'end_date': end_date, 'game_map': game_map, 'game_type': game_type, 'player': player, 'min_id': min_id}
-        r = requests.get("https://hotsapi.net/api/v1/replays/paged", params = payload)
-        return self.return_json(r)
-
-    def get_replay(self, id):
-        r = requests.get("https://hotsapi.net/api/v1/replays/" + str(id))
+    def get_replay_list(self, min_id = None, existing = False, with_players = False):
+        payload = {'min_id': min_id, 'existing': existing, 'with_players': with_players}
+        r = requests.get("https://hotsapi.net/api/v1/replays", params = payload)
         return self.return_json(r)
 
     def upload_replay(self, file):
         file = {"file": open(file, "rb")}
-        r = requests.post("https://hotsapi.net/api/v1/replays/", files = file)
+        r = requests.post("https://hotsapi.net/api/v1/replays", files = file)
+        return self.return_json(r)
+
+    def get_parsed_replay_list(self, min_parsed_id = None, with_players = False):
+        payload = {'min_parsed_id': min_parsed_id, 'with_players': with_players}
+        r = requests.get("https://hotsapi.net/api/v1/replays/parsed", params = payload)
+        return self.return_json(r)
+    
+    def get_replay(self, id):
+        r = requests.get("https://hotsapi.net/api/v1/replays/" + str(id))
         return self.return_json(r)
 
     def get_hero_list(self):
-        r = requests.get("https://hotsapi.net/api/v1/heroes/translations")
+        r = requests.get("https://hotsapi.net/api/v1/heroes")
+        return self.return_json(r)
+
+    def get_hero(self, hero):
+        r = requests.get("https://hotsapi.net/api/v1/heroes/" + str(hero))
+        return self.return_json(r)
+
+    def get_hero_ability(self, hero, hotkey):
+        r = requests.get("https://hotsapi.net/api/v1/heroes/" + str(hero) + "/abilities/" + str(hotkey))
+        return self.return_json(r)
+    
+    def get_talent(self, talent):
+        r = requests.get("https://hotsapi.net/api/v1/talents/" + str(talent))
         return self.return_json(r)
 
     def get_map_list(self):
-        r = requests.get("https://hotsapi.net/api/v1/maps/translations")
+        r = requests.get("https://hotsapi.net/api/v1/maps")
         return self.return_json(r)
+
+    def get_map(self, map):
+        r = requests.get("https://hotsapi.net/api/v1/maps/" + str(map))
